c07999c007388953d771608ff834432632a52874
wiselabs <c_lucas1010@hotmail.com>
2015-07-30 08:02:07 -0300

    finalizado percolation

--- coursera-princeton/ProgrammingAssgnmentPercolation/src/Percolation.java
+++ coursera-princeton/ProgrammingAssgnmentPercolation/src/Percolation.java
@@ -1,4 +1,4 @@
-import java.time.YearMonth;
+
 
 /**
  * @author C.Lucas <christoffer.luccas@gmail.com>
@@ -7,17 +7,17 @@ import java.time.YearMonth;
 
 public class Percolation {
 	private int order	//	order of matrix
-	,TNode 				//	virtual node top
-	,BNode;				//	virtual node bottom
+	,tNode 				//	virtual node top
+	,bNode;				//	virtual node bottom
 	private WeightedQuickUnionUF wqu, wqu2;		// quick union objects
-	private boolean sites[];					// arrays representing sites open or blocking
+	private boolean[] sites;					// arrays representing sites open or blocking
 	
 	/**
 	 * constructor
 	 * @param n order of matrix
 	 * */
 	
-	public Percolation (int n) {
+	public Percolation(int n) {
 		if(n <= 0)
 			throw new java.lang.IllegalArgumentException("N should be > 0");
 		this.order = n;
@@ -25,8 +25,8 @@ public class Percolation {
 		// logo teremos indices de 1 a N inclusive
 		wqu = new WeightedQuickUnionUF(order * order + 2);
 		wqu2 = new WeightedQuickUnionUF(order * order + 2);
-		TNode = order * order;
-		BNode = order * order + 1;
+		tNode = order * order;
+		bNode = order * order + 1;
 		sites = new boolean[order * order];	//[0 .. (order - 1(]		
 	}
 	// igual ao metodo union(p,q)
@@ -47,11 +47,11 @@ public class Percolation {
 	public boolean isFull(int p, int q) {
 		validate(p, q);
 		int idx = idx2Dto1D(p, q);
-		return wqu2.connected(idx, TNode);
+		return wqu2.connected(idx, tNode);
 	}
 	
 	public boolean percolates() {
-		return wqu.connected(TNode, BNode);
+		return wqu.connected(tNode, bNode);
 	}
 	
 	/**
@@ -76,9 +76,9 @@ public class Percolation {
 	
 	private void connectSites(int p, int q) {
 		int site = idx2Dto1D(p, q);
-      
+
 		// look left and right
-		if (q >= 1 && q <= order ) {
+		if (q >= 1 && q <= order && order > 1) {
 			if(q == 1) {
 	        	if(isOpen(p, q+1))
 	        		union(site, idx2Dto1D(p, q+1));	
@@ -96,20 +96,20 @@ public class Percolation {
 		// look top and bottom
         if (p >= 1 && p <= order){
         	if(p == 1) {
-        		union(site, TNode);
+        		union(site, tNode);
         	} else if(p == order) {
-        		union(site, BNode);
+        		union(site, bNode);
         	}
-            if (p > 1 && isOpen(p-1, q)) 
+            if (p > 1 && isOpen(p-1, q) && order > 1) 
                 union(site, idx2Dto1D(p-1, q));
-            if (p < order && isOpen(p+1, q))
+            if (p < order && isOpen(p+1, q) && order > 1)
 				union(site, idx2Dto1D(p+1, q));
         }
 	}
 	
 	private void union(int p, int q) {
 		wqu.union(p, q);
-		if(q != BNode)
+		if(q != bNode)
 			wqu2.union(p, q);
 	}
 	
@@ -137,6 +137,10 @@ public class Percolation {
 			,"http://coursera.cs.princeton.edu/algs4/testing/percolation/input20.txt"
 			,"http://coursera.cs.princeton.edu/algs4/testing/percolation/jerry47.txt"
 			,"http://coursera.cs.princeton.edu/algs4/testing/percolation/wayne98.txt"
+			,"http://coursera.cs.princeton.edu/algs4/testing/percolation/input1.txt"
+			,"http://coursera.cs.princeton.edu/algs4/testing/percolation/input1-no.txt"
+			,"http://coursera.cs.princeton.edu/algs4/testing/percolation/input2.txt"
+			,"http://coursera.cs.princeton.edu/algs4/testing/percolation/input2-no.txt"
 		};
 		//BinaryIn bin = new BinaryIn(url[0]);
 		//byte s = ! bin.isEmpty() ? bin.readByte() : 0;
@@ -145,7 +149,7 @@ public class Percolation {
 		//for(String url : URLS){}
 		int DELAY = 5;
         
-		In in = new In(URLS[5]);       // input file
+		In in = new In(URLS[9]);       // input file
         int N = in.readInt();         // N-by-N percolation system
         // turn on animation mode
         StdDraw.show(0);
--- coursera-princeton/ProgrammingAssgnmentPercolation/src/PercolationStats.java
+++ coursera-princeton/ProgrammingAssgnmentPercolation/src/PercolationStats.java
@@ -1,30 +1,99 @@
 
+
 public class PercolationStats {
 	/**
 	 * @param n integer representing the size of a matrix
 	 * @param t integer representing 
 	 * */
+	private static final double T = 1.96;
+	private double[] results;
+	
 	public PercolationStats(int n, int t) {
 		if(n <= 0 || t <= 0)
-			throw new IllegalArgumentException("");
+			throw new IllegalArgumentException("Illegal Argument");
+		results = setResults(n, t);
+	}
+	
+	private double[] setResults(int n, int t) {
+		results = new double[t];
+		for(int i=0; i<t; i++){
+			int counter = 0;
+			Percolation perc = new Percolation(n);
+			while(!perc.percolates()){
+				int q;
+				int p;
+				do {
+					p = StdRandom.uniform(1, n+1);
+					q = StdRandom.uniform(1, n+1);
+				} while(perc.isOpen(p, q));
+				perc.open(p, q);
+				counter++;
+			}
+			results[i] = ((double) counter) / (n*n); 
+		}
+		return this.results;
+	}
+	
+	private double[] getResults() {
+		return this.results;
+	}
+	
+	private double confidenceInterval() {
+		return T * stddev() / Math.sqrt(results.length);
 	}
 	
 	public double mean() {
-		return 0.0;
+		return StdStats.mean(getResults());
 	}
 	
 	public double stddev() {
-		return 0.0;
+		return StdStats.stddev(getResults());
 	}
 	
 	public double confidenceLo() {
-		return 0.0;
+		return mean() - confidenceInterval();
 	}
 	public double confidenceHi() {
-		return 0.0;
+		return mean() + confidenceInterval();
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
+		int n = 0 , t = 0;
+		/*
+		if(args.length == 2) {
+			n = Integer.parseInt(args[0]);
+			t = Integer.parseInt(args[1]);
+		}
+		*/
+		/**
+		In in = new In();
+		String line = in.readLine();
+		String number[] = line.split(" ");
+		*/
+	}
+	
+	private static void teste(int n, int t) {
 		
+		PercolationStats pStats = new PercolationStats(n, t);
+		double mean = pStats.mean();
+		double stddev = pStats.stddev();
+		double cLow = pStats.confidenceLo();
+		double cHigh = pStats.confidenceHi();
+		/*
+		String fmt = String.format("mean %18s = %f\nstddev %18s = %f\n95% confidence interval = %f, %f",
+				" ", mean, " ", stddev, cLow, cHigh);
+				*/
+		
+		String fmt = String.format("mean %17s = %s\nstddev %15s = %s\n95 confidence interval = %s, %s", 
+				" ",
+				String.valueOf(mean).replace(',', '.'), " ",
+				String.valueOf(stddev).replace(',', '.'),
+				String.valueOf(cLow).replace(',', '.'),
+				String.valueOf(cHigh).replace(',', '.'));
+		String fmt2 = String.format("mean %17s = %.10f\nstddev %15s = %.10f\n95 confidence interval = %.10f, %.10f", 
+				" ",mean, " ",stddev,cLow,cHigh);
+		
+		StdOut.println(fmt);
+		StdOut.println(fmt2);
 	}
 }
