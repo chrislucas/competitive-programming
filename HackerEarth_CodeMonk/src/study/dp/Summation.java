42fda0d24826ed666325a67777951ea86d92caf0
wiselabs <christoffer.luccas@gmail.com>
2015-11-11 09:19:34 -0200

    estudo sobre programacao dinamica

--- HackerEarth_CodeMonk/src/study/dp/Summation.java
+++ HackerEarth_CodeMonk/src/study/dp/Summation.java
@@ -1,5 +1,9 @@
 package study.dp;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Summation {
 	
 	/**
@@ -10,24 +14,48 @@ public class Summation {
 
 	public static int solution(int[] coins, int S) {
 		int limit = coins.length;
-		int [] M = new int[S];
-		for(int i=0; i<S; i++) {
-			M[i] = Integer.MAX_VALUE;
+		int [] SET = new int[S+1];
+		for(int i=0; i<S+1; i++) {
+			SET[i] = Integer.MAX_VALUE;
 		}
-		M[0] = 0;
-		for(int i=1; i<S; i++) {
+		SET[0] = 0;
+		for(int i=1; i<=S; i++) {
 			for(int j=0; j<limit; j++) {
 				int v = coins[j];
-				if(v <= 1 && M[i - v] + 1 < M[i]) {
-					 M[i] = M[i - v] + 1;
+				if(v <= i && SET[i-v]+1 < SET[i]) {
+					SET[i] = SET[i-v]+1;
 				}
 			}
 		}
-		return M[S-1];
+		return SET[S];
+	}
+	/**
+	 * Retorna um mapa<Soma, Moedas usadas>
+	 * */
+	public static Map<Integer, List<Integer>> f(int [] coins, int sum) {
+		Map<Integer, List<Integer>> map = new HashMap<>();
+		return map;
+	}
+	
+	/**
+	 * Menor subsequencia crescente (longest increase subsequence)
+	 * http://www.ime.usp.br/~pf/analise_de_algoritmos/aulas/sscm.html
+	 * */
+	
+	public static int lisFx(int [] SET) {
+		int answer = 1;
+		int [] memo = new int[SET.length];
+		for(int i=0; i<SET.length; i++)
+			memo[i] = Integer.MAX_VALUE;
+		memo[0] = 1;
+		for(int i=0; i<SET.length; i++) {
+			
+		}
+		return answer;
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(solution(new int[]{1,3,5}, 11));
+		System.out.println(solution(new int[]{1,3,5,11}, 23));
 
 	}
 
