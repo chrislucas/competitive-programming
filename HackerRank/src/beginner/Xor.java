3d8732226df55bf5a924b5dae0418c2a044042a9
christoffer <christoffer.luccas@gmail.com>
2015-12-10 15:04:43 -0200

    criando class somente para commitar para quebrar record, depois faço o exercicio

---
+++ HackerRank/src/beginner/Xor.java
@@ -0,0 +1,11 @@
+package beginner;
+
+// https://www.hackerearth.com/tracks/pledge-2015-easy/panda-and-xor/
+public class Xor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
