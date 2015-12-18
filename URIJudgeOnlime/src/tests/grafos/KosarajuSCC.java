db14bf14fa4ceddc89c797d3d8bdac3a6437ff1e
wiselabs <christoffer.luccas@gmail.com>
2015-12-18 12:46:02 -0200

    estudo, componentes fortemente conexos e 2-sAT

--- URIJudgeOnlime/src/tests/grafos/KosarajuSCC.java
+++ URIJudgeOnlime/src/tests/grafos/KosarajuSCC.java
@@ -1,35 +1,34 @@
 package tests.grafos;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 import java.util.Stack;
 
 /**
  * http://www.geeksforgeeks.org/strongly-connected-components/
  * SCC: Strongly connected component
- * Componente fortemente conexo 
+ * Componente fortemente conexo
+ * https://en.wikipedia.org/wiki/Kosaraju%27s_algorithm
  * */
 
 public class KosarajuSCC {
 	
-	public static Map<Integer, ArrayList<Integer>> map;
+	public static ArrayList<ArrayList<Integer>> map;
 	public static Set<ArrayList<Integer>> components;
 	public static int vertices;
 	
+	public static ArrayList<ArrayList<Integer>> create() {
+		ArrayList<ArrayList<Integer>> adjacent = new ArrayList<>();
+		for(int i=0; i<vertices; i++)
+			adjacent.add(new ArrayList<>());
+		return adjacent;
+	}
+	
+	
 	static void addEdge(int u, int v) {
-		if(map.containsKey(u)) {
-			map.get(u).add(v);
-		} else {
-			ArrayList<Integer> array = new ArrayList<>();
-			array.add(v);
-			map.put(u, array);
-		}
-		
+		map.get(u).add(v);
 	}
 	
 	public static void fill(int v, boolean[] visited, Stack<Integer> stack) {
@@ -42,61 +41,69 @@ public class KosarajuSCC {
 		}
 		stack.push(v);
 	}
+	
+	public static void dfs(int u, boolean[] visited, ArrayList<ArrayList<Integer>> reverse) {
+		visited[u] = true;
+		System.out.printf("%d ", u);
+		Iterator<Integer> it = reverse.get(u).iterator();
+		while(it.hasNext()) {
+			int v = it.next();
+			if( ! visited[v] )
+				dfs(v, visited, reverse);
+		}
+	}
 
 	public static void kosaraju(int v) {
-		Stack<Integer> stack = new Stack<>();
-		components = new HashSet<>();
-		
+		vertices = v;
+		map = create();
+		addEdge(0, 2);
+		addEdge(0, 3);
+		addEdge(1, 0);
+		addEdge(2, 1);
+		addEdge(3, 4);
+		addEdge(2, 1);
 		boolean[] visited = new boolean[v];
 		for(int i=0; i<v; i++) {
 			visited[i] = false;
-		}	
-		map = new HashMap<>();
-		
+		}
+		Stack<Integer> stack = new Stack<>();
 		for(int i=0; i<v; i++) {
 			if(!visited[i]) {
 				fill(i, visited, stack);
 			}
 		}
-		
-		Map<Integer, ArrayList<Integer>> reverse = transpose(map);
+		ArrayList<ArrayList<Integer>> reverse = transpose(map);
 		
 		for(int i=0; i<v; i++)
 			visited[i] = false;
 		
+		//components = new HashSet<>();
+		
 		while(!stack.empty()) {
 			int p = stack.pop();
 			if( ! visited[p] ) {
-				dfs(p, visited);
+				dfs(p, visited, reverse);
+				System.out.println("");
 			}
 		}
 	}
 	
-	public static Map<Integer, ArrayList<Integer>> transpose(Map<Integer, ArrayList<Integer>> map) {
-		Map<Integer, ArrayList<Integer>> reverse = new HashMap<>();
+	// invertendo uma lista de adjacencia
+	public static ArrayList<ArrayList<Integer>> transpose(ArrayList<ArrayList<Integer>> list) {
+		ArrayList<ArrayList<Integer>> reverse = create();
 		for(int i=0; i<vertices; i++) {
-			@SuppressWarnings("rawtypes")
-			Iterator it = map.get(i).iterator();
-			while(it.hasNext()) {
-
+			ArrayList<Integer> array = list.get(i); 
+			for(Integer vs : array) {
+				reverse.get(vs).add(i);
 			}
 		}
-
 		return reverse;
 	}
 	
-	public static void dfs(int u, boolean[] visited) {
-		visited[u] = true;
-		Iterator<Integer> it = map.get(u).iterator();
-		while(it.hasNext()) {
-			int v = it.next();
-			if( ! visited[v] )
-				dfs(v, visited);
-		}
-	}
+
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		kosaraju(5);
 
 	}
 
--- URIJudgeOnlime/src/tests/math/SAT2.java
+++ URIJudgeOnlime/src/tests/math/SAT2.java
@@ -6,12 +6,8 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Map;
+import java.util.Iterator;
 import java.util.Stack;
-
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
-
-import sun.util.locale.StringTokenIterator;
 
 /**
  * http://danielamaral.wikidot.com/paa-2007-1-projeto-1-componentes-fortemente-conectados#toc15
@@ -20,6 +16,7 @@ import sun.util.locale.StringTokenIterator;
  * http://pastebin.com/BfRpi1Tw
  * https://icpcarchive.ecs.baylor.edu/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=887
  * */
+
 // http://codeforces.com/blog/entry/16205
 // https://kartikkukreja.wordpress.com/2013/05/16/solving-2-sat-in-linear-time/
 // http://www.cse.psu.edu/~kasivisw/2sat.pdf
@@ -28,6 +25,9 @@ public class SAT2 {
 	public static String[] literals;
 	public static ArrayList<ArrayList<Integer>> adjacent;
 	public static boolean [] visited;
+	
+	//union-find
+	public static int [] uf;
 
 	public static int vertices;
 	// esse inteiro vai representar uma variavel da expressao booleana
@@ -45,20 +45,13 @@ public class SAT2 {
 	 * 
 	 * */
 	public static int addLiteral(String literal) {
-		int answer = 0;
-		if(nextChar == 0) {
-			answer = nextChar;
-			literals[answer] = literal;
-			nextChar++;
-		}
-			
+		int answer = 0;			
 		for(int i=0; i<nextChar; i++) {
 			if(literals[i].equals(literal)) {
 				answer = i;
 				break;
 			}
 		}
-		
 		if(answer == 0) {
 			answer = nextChar;
 			literals[answer] = literal;
@@ -71,7 +64,7 @@ public class SAT2 {
 	public static void cleanAdjacent() {
 		adjacent 	= create(); 
 		visited 	= new boolean[vertices];
-		literals 	= new String[2*vertices];
+		literals 	= new String[vertices];
 	}
 	
 	public static ArrayList<ArrayList<Integer>> create() {
@@ -84,11 +77,9 @@ public class SAT2 {
 	public static void addEdge(int u, int v) {
 		adjacent.get(u).add(v);
 	}
-	
 	// se o literal comecar com !, ele representa uma negacao (~A)
-	public static boolean valueLiteral(String literal) {
-		return literal.charAt(0) == '!';
-	}
+	// public static boolean valueLiteral(String literal) { return literal.charAt(0) == '!'; }
+	
 	// invertendo uma lista de adjacencia
 	public static ArrayList<ArrayList<Integer>> transpose(ArrayList<ArrayList<Integer>> list) {
 		ArrayList<ArrayList<Integer>> reverse = create();
@@ -102,7 +93,7 @@ public class SAT2 {
 	}
 	
 	public static void runTest() {
-		String la, lb, enter;
+		String enter;
 		BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
 		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
 		try {
@@ -131,19 +122,47 @@ public class SAT2 {
 				}
 				
 				Stack<Integer> stack = new Stack<>();
+				fill(vertices, visited, stack, 0);
+				ArrayList<ArrayList<Integer>> reverse = transpose(adjacent);
+				for(int i=0; i<vertices; i++)
+					visited[i] = false;
+				while(!stack.empty()) {
+					int p = stack.pop();
+					if( ! visited[p] ) {
+						dfs(p, visited, reverse);
+					}
+				}
 			}
 		} catch(IOException ioex){}
 	}
 	
+	// prencher a pilha com os vertices raiz dos componentes fortemente conexos
+	public static void fill(int v, boolean[] visited, Stack<Integer> stack, int acc) {
+		visited[v] = true;
+		Iterator<Integer> it = adjacent.get(v).iterator();
+		while(it.hasNext()) {
+			int n = (Integer)it.next();
+			if( ! visited[n] )
+				fill(n, visited, stack, acc++);
+		}
+		stack.push(v);
+	}
+	
 	/**
-	 * Usar o algoritmo de tarjan
+	 * Depois Testar o algoritmo de tarjan
 	 * */
-	public static void dfs(int source) {
-		
+	public static void dfs(int u, boolean[] visited, ArrayList<ArrayList<Integer>> reverse) {
+		visited[u] = true;
+		Iterator<Integer> it = reverse.get(u).iterator();
+		while(it.hasNext()) {
+			int v = it.next();
+			if( ! visited[v] )
+				dfs(v, visited, reverse);
+		}
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		runTest();
 
 	}
 
