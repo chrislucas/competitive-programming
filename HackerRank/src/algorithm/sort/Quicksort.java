83fee834ee048030fc54763f9a99250ba93eac22
wiselabs <christoffer.luccas@gmail.com>
2016-03-01 08:47:26 -0300

    estudos

--- HackerRank/src/algorithm/sort/Quicksort.java
+++ HackerRank/src/algorithm/sort/Quicksort.java
@@ -1,5 +1,11 @@
 package algorithm.sort;
 
+
+/*
+ * https://www.hackerrank.com/challenges/quicksort2
+ * 
+ * */
+
 public class Quicksort {
 	
 	public static <T extends Comparable<T>> void sort(T [] set, int left, int right) {
@@ -24,12 +30,26 @@ public class Quicksort {
 		if(i < right)
 			sort(set, i, right);
 	}
+	
+	public static int partition(Comparable [] set, int lo, int hi) {
+		return 0;
+	}
+	
+	public static void qSort(Comparable [] set, int lo, int hi) {
+		if(hi<=lo)
+			return;
+		int i = partition(set, lo, hi);
+		sort(set, lo, i-1);
+		sort(set,i+1, hi);
+	}
+	
 
 	public static void main(String[] args) {
 		Character [] set = {'a', 'd', 'c'}; 
 		sort(set, 0, set.length);
 		for(Character c : set)
 			System.out.println(c);
+		qSort(set, 0, set.length-1);
 	}
 
 }
