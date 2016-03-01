22452d27eb3ab24b7a17afa616d263403b980374
christoffer <christoffer.luccas@gmail.com>
2016-03-01 16:19:06 -0300

    estudo funcoes

---
+++ URIJudgeOnlime/src/test/geo/EstudoTrigonometria.java
@@ -0,0 +1,25 @@
+package test.geo;
+
+public class EstudoTrigonometria {
+
+	
+	static class Point2D {
+		double x, y;
+		public Point2D() {}
+		public Point2D(double x, double y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		public double getAngle(Point2D that) {
+			return 0.0;
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
