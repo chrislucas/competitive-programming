62a525379e82109ce4dc9369230b703995f657a6
christoffer <christoffer.luccas@gmail.com>
2016-03-12 17:09:02 -0300

    estudo sobre quicksort

---
+++ HackerRank/src/challenges/StudyQS.java
@@ -0,0 +1,21 @@
+package challenges;
+
+public class StudyQS {
+	
+	// https://www.hackerrank.com/challenges/quicksort2
+	
+	public static <T> void swap(T [] set, int a, int b) {
+		T aux = set[a];
+		set[a] = set[b];
+		set[b] = aux;
+		return;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
