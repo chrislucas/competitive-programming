bc15e569cf56199ae5cf77848781990e196fe0ce
wiselabs <christoffer.luccas@gmail.com>
2016-05-18 12:49:11 -0300

    bla bla bla

---
+++ SpojBR/src/math/HNumeros.java
@@ -0,0 +1,14 @@
+package math;
+
+/**
+ * http://br.spoj.com/problems/HNUMBER/
+ * */
+
+public class HNumeros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
