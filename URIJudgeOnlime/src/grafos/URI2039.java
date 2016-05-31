6b63d610e73eaee357d03ef149bbb30f22b89564
wiselabs <christoffer.luccas@gmail.com>
2016-05-31 09:44:40 -0300

    proximos estudos

---
+++ URIJudgeOnlime/src/grafos/URI2039.java
@@ -0,0 +1,14 @@
+package grafos;
+
+/*
+ * https://www.urionlinejudge.com.br/judge/pt/problems/view/2039
+ * */
+
+public class URI2039 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
