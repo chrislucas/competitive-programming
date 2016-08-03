bae472ddb399fcf9f1ed7ac7ae30018fcd7bd4c3
wiselabs <christoffer.luccas@gmail.com>
2016-08-03 09:27:11 -0300

    cccccc

---
+++ HackerRank/src/math/fundamentals/MaximumDraws.java
@@ -0,0 +1,18 @@
+package math.fundamentals;
+
+/*
+ * https://www.hackerrank.com/challenges/maximum-draws
+ * */
+
+public class MaximumDraws {
+
+	public static void solver() {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
