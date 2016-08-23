77576bc9eb1a06fb853c739e2520b1f025c926a0
wiselabs <christoffer.luccas@gmail.com>
2016-08-23 09:25:57 -0300

    ccccc

--- URIJudgeOnlime/src/math/URI1492.java
+++ URIJudgeOnlime/src/math/URI1492.java
@@ -5,27 +5,17 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 // https://www.urionlinejudge.com.br/judge/pt/problems/view/1492
 public class URI1492 {
 	
 	static long [] table;
-	
-	public static long solution(long a, long b) {
-		return table[Long.bitCount(b)] - table[Long.bitCount(a)];
-	}
-	
-	public static void init(){
-		int lim = 1000000;
-		table = new long[lim];
-		table[0] = 0;
-		for(int i=1; i<lim; i++) {
-			table[i] = Long.bitCount(i) + table[i-1];
-		}
-		return;
-	}
-	
+	static Map<BigInteger, Integer> map;
+		
 	public static long bitCount(BigInteger num) {
 		long counter = 0;
 		while( num.compareTo(BigInteger.ONE) >= 0  ) {
@@ -41,15 +31,28 @@ public class URI1492 {
 		return counter;
 	}
 	
+	public static void initialize() {
+		map = new HashMap<>();
+		map.put(BigInteger.ZERO, 0);
+		BigInteger idx = BigInteger.ONE;
+		BigInteger lim = new BigInteger("10000000000000000");
+		while(idx.compareTo(lim) == -1) {
+			map.put(idx, idx.bitCount());
+			idx = idx.add(BigInteger.ONE);
+		}
+		return;
+	}
+	
 	public static void runTest() {
+		initialize();
 		//10000000000000000
 		BigInteger b = new BigInteger("10000000000000000");
 		int count = b.bitCount();
-		System.out.printf("%d %d", count, bitCount(b));
+		System.out.printf("%d %d\n", count, bitCount(b));
 	}
 	
 	public static void main(String[] args) {
-		init();
+		//init();
 		runTest();
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		PrintWriter writer = new PrintWriter(new OutputStreamWriter(System.out), true);
@@ -59,7 +62,7 @@ public class URI1492 {
 				StringTokenizer token = new StringTokenizer(in, " ");
 				long a = Long.parseLong(token.nextToken());
 				long b = Long.parseLong(token.nextToken());
-				writer.printf("%d\n", solution(a, b));
+				writer.printf("%d\n");
 			}
 		} catch(Exception ex) {}
 
