36735a751802866da47b9275efa33c21389cdf12
wiselabs <christoffer.luccas@gmail.com>
2016-08-30 09:46:03 -0300

    commit

---
+++ URIJudgeOnlime/src/paradigmas/dp/URI1282.java
@@ -0,0 +1,14 @@
+package paradigmas.dp;
+
+/*
+ * https://www.urionlinejudge.com.br/judge/pt/problems/view/1282
+ * */
+
+public class URI1282 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ hackerearth/src/com/br/practice/track/basic/CastleGate.java
@@ -0,0 +1,14 @@
+package com.br.practice.track.basic;
+
+/*
+ * https://www.hackerearth.com/problem/algorithm/the-castle-gate-july-easy/
+ * */
+
+public class CastleGate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
