7fe26c6815407b3031c9f917376a9d57825afcd0
wiselabs <christoffer.luccas@gmail.com>
2017-01-30 03:52:52 -0200

    estudo sobre algoritmo de segmentacao de imagens binarias

--- HackerRank/src/ai/digitalimageprocess/ImgSegm1.java
+++ HackerRank/src/ai/digitalimageprocess/ImgSegm1.java
@@ -6,40 +6,9 @@ import java.util.ArrayList;
  * https://en.wikipedia.org/wiki/Connected-component_labeling
  * http://homepages.inf.ed.ac.uk/rbf/HIPR2/connect.htm
  * https://www.codeproject.com/Articles/336915/Connected-Component-Labeling-Algorithm
+ * http://www.eng.tau.ac.il/~danar/Miscellaneous/rotem-project.pdf
  * */
 public class ImgSegm1 {
-		
-	public static byte grid [][] = {
-		 {0,0,0,1,1,0,0,0,1,0,1,0}
-		,{1,1,1,0,1,1,1,1,0,0,0,1}
-		,{1,1,1,0,1,0,0,1,0,0,1,0}
-		,{1,0,0,0,0,0,0,0,0,1,0,0}
-	};
-	
-	public static int limX = grid.length;
-	public static int limY = grid[0].length;
-	
-	public static boolean validate(int x, int y) {
-		if(x >= limX || y >= limY || x < 0 || y < 0)
-			return false;
-		
-		return true;
-	}
-	
-	public static ArrayList<Point2D> is4connected(int x, int y) {
-		ArrayList<Point2D> points = new ArrayList<>();
-		if( ! validate(x, y) )
-			return points;
-		
-		int X [] = {1,-1,0,0};
-		int Y [] = {0,0,1,-1};
-
-		for(int i=0; i<X.length; i++) {
-			if( ! validate(x+X[i], y+Y[i]) )
-				points.add(new Point2D(x+X[i], y+Y[i]));
-		}
-		return points;
-	}
 	
 	public static class Point2D {
 		int x, y;
@@ -49,14 +18,46 @@ public class ImgSegm1 {
 		}
 	}
 	
-	public static void runTest4connectivity() {
+	public static byte grid [][] = {
+		 {0,0,0,1,1,0,0,0,1,0,1,0}
+		,{1,1,1,0,1,1,1,1,0,0,0,1}
+		,{1,1,1,0,1,0,0,1,0,0,1,0}
+		,{1,0,0,0,0,0,0,0,0,1,0,0}
+	};
+	
+	public static int limX = grid.length;
+	public static int limY = grid[0].length;
+	public static boolean visited[][] = new boolean[limX][limY];
+	public static int X [] = {1,-1,0,0};
+	public static int Y [] = {0,0,1,-1};
+	
+	public static boolean validate(int x, int y) {
+		if(x >= limX || y >= limY || x < 0 || y < 0)
+			return false;
+		return true;
+	}
+	
+	public static ArrayList<Point2D> neighbours(int x, int y) {
+		ArrayList<Point2D> points = new ArrayList<>();
+		for(int i=0; i<X.length; i++) {
+			if( ! validate(x+X[i], y+Y[i]) )
+				points.add(new Point2D(x+X[i], y+Y[i]));
+		}
+		return points;
+	}
+	
+	public static void dfsGrid(int i, int j) {
+		ArrayList<Point2D> points = neighbours(i, j);
+		for(Point2D point : points) {
+			int x = point.x, y = point.y;
+		}
+	}
+	
+	public static void run() {
 		for(int i=0; i<limX; i++) {
 			for(int j=0; j<limY; j++){
-				if(grid[i][j] == 0)
-					continue;
-				ArrayList<Point2D> points = is4connected(i, j);
-				for(Point2D point : points) {
-					
+				if(grid[i][j] == 1 && !visited[i][j]) {
+
 				}
 			}
 		}
