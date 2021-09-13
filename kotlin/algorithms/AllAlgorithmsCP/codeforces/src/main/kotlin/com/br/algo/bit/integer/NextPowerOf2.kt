f03418ec2494e774eefecedcf56abbb2bf22c431
chrislucas <christoffer.luccas@gmail.com>
2021-09-13 01:49:54 -0300

    fodasse

---
+++ kotlin/algorithms/AllAlgorithmsCP/codeforces/src/main/kotlin/com/br/algo/bit/integer/NextPowerOf2.kt
@@ -0,0 +1,7 @@
+package com.br.algo.bit.integer
+
+/**
+ * https://stackoverflow.com/questions/466204/rounding-up-to-next-power-of-2
+ * https://en.wikipedia.org/wiki/Logarithm#Change_of_base
+ * https://www.hackerearth.com/practice/notes/round-a-number-to-the-next-power-of-2/
+ * */
