f85d6d949c7aebff671ea10b5934e0875ecd1d82
chrislucas <christoffer.luccas@gmail.com>
2021-10-03 10:09:05 -0300

    ëstudos

--- kotlin/algorithms/AllAlgorithmsCP/src/com/br/kotlin/range/SampleClosedRange.kt
+++ kotlin/algorithms/AllAlgorithmsCP/src/com/br/kotlin/range/SampleClosedRange.kt
@@ -1,7 +1,5 @@
 package src.com.br.kotlin.range
 
-import java.lang.StringBuilder
-
 /**
  * https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.ranges/-closed-range/
  *
@@ -10,21 +8,32 @@ import java.lang.StringBuilder
  *  * Intervalos
  * https://www.infoescola.com/matematica/intervalo/
  *
- * Intervalos abertos
+ * INTERVALOS ABERTOS
  *
  * Intervalo aberto ] x, y [ = {q  existe nos Reais: x < q < y }
- * dizemos que um intervalo eh aberto quando seus quando seus extremos nao estao incluidos
+ * dizemos que um intervalo eh aberto  quando seus extremos nao estao incluidos
  *
  * x = -5, y = 0 -> x < q < y -> ]-5, 0 [ -> S = {q Existe N: x < q < y}
  *
- * intervalo aberto indidando um dos extremos ] x, +inf [ (direita), ]-inf, y[ (esqyerda)
- * intervalo aberto a direita ] x, +inf [ -> {q Existe R : q > x }
- * intervalo aberto a esquerda ] -inf, y [ -> {q Existe R: q < y}
+ * intervalo aberto indidando um dos extremos
+ *  -> ] x, +inf [ aberto a DIREITA
+ *  -> ] -inf, y [ aberto a ESQUERDA
+ * intervalo aberto a direita ] x, +inf [ -> {q Existe Reais : q > x }
+ * intervalo aberto a esquerda ] -inf, y [ -> {q Existe Reais : q < y}
  *
- * Intervalo fechado: Aquele em que seus extremos sao inclusidos
- * [a, b] =  { x Existe R : a <= x <= b }
+ * INTERVALO FECHADO: Aquele em que seus extremos sao inclusidos
+ * [a, b] = { x Existe R : a <= x <= b }
  *
- * Intervalo semiaberto: Aquele cujo um dos extremos nao esta incluido
+ * INTERVALO SEMI-ABERTO ou SEMI-FECHADO: Aquele cujo um dos extremos é incluido
+ *
+ * [ a, b [ = semi aberto  a direita = {x E R: a <= x < b}
+ * // com extremos infinito
+ * [ a, +INF [ = semi aberto  a direita = {x E R: a <= x }
+ *
+ * ] a, b ] = semi aberto  a esquerda = {x E R: a < x <= b}
+ *
+ * // com extremos infinito
+ * ] -INF, b ] = semi aberto  a esquerda = {x E R: x <= b}
  *
  *
  *
@@ -43,22 +52,67 @@ import java.lang.StringBuilder
  *
  * */
 
-class MyComparableClosedRange<T : Comparable<T>>(override val endInclusive: T, override val start: T) : ClosedRange<T>
+class GenClosedRange<T : Comparable<T>>(
+    override val start: T,
+    override val endInclusive: T
+) : ClosedRange<T> {
+    override fun toString(): String = "$start .. $endInclusive"
+}
 
+
+private fun checkMyComparableClosedRange() {
+    val range = GenClosedRange(1, 20)
+    println(range)
+    println(range.contains(19))
+
+    val charRange = GenClosedRange('a', 'z')
+    println(charRange)
+    println(charRange.contains('c'))
+}
 
 private fun checkCharRange() {
     /**
      * definicao da classe CharRange
-     * class CharRange(start: Char, endInclusive: Char) : CharProgression(start, endInclusive, 1), ClosedRange<Char>
+     * class CharRange(start: Char, endInclusive: Char) :
+     *  CharProgression(start, endInclusive, 1), ClosedRange<Char>
      *
      * */
-    val range = ('a' .. 'z')
+    val range = ('a'..'z')
     val reversedRange = range.reversed()
     println(range.joinTo(StringBuilder(), separator = "|").toString())
     println(reversedRange)
     println(reversedRange.joinTo(StringBuilder(), separator = "|").toString())
 }
 
+private fun checkIntRange() {
+    val range = (1..25)
+    val reversedRange = range.reversed()
+    println("$range\n$reversedRange")
+    println(range.joinTo(StringBuilder(), separator = "|").toString())
+    println(reversedRange.joinTo(StringBuilder(), separator = "|").toString())
+}
+
+private fun checkIntRangeWithStep() {
+    val range = (1..25 step 5)
+    val reversedRange = range.reversed()
+    println("$range\n$reversedRange")
+    println(range.joinTo(StringBuilder(), separator = "|").toString())
+    println(reversedRange.joinTo(StringBuilder(), separator = "|").toString())
+
+
+    val anotherRange = (100 downTo 25 step 6)
+    println(anotherRange.joinTo(StringBuilder(), separator = "|").toString())
+
+}
+
+private fun checkCharRangeWithStep() {
+    val range = ('z' downTo 'a' step 5)
+    val reversedRange = range.reversed()
+    println("$range\n$reversedRange")
+    println(range.joinTo(StringBuilder(), separator = "|").toString())
+    println(reversedRange.joinTo(StringBuilder(), separator = "|").toString())
+}
+
 fun main() {
-    checkCharRange()
+    checkMyComparableClosedRange()
 }
