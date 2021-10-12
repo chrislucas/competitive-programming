7396528ead602efc08e567823dc31c9bcf3b7d21
chrislucas <christoffer.luccas@gmail.com>
2021-10-12 16:17:30 -0300

    estudando bitwise

---
+++ kotlin/algorithms/AllAlgorithmsCP/src/com/br/cp/book/cphandbook/chp10/cbuiltin/cbuiltin.kt
@@ -0,0 +1,29 @@
+package src.com.br.cp.book.cphandbook.chp10.cbuiltin
+
+/**
+
+Competitive Programmer’s Handbook
+Antti Laaksonen
+Draft July 3, 2018
+
+ * */
+
+val Int.clz: Int
+    get() {
+        return 0
+    }
+
+val Int.ctz: Int
+    get() {
+        return 0
+    }
+
+val Int.popcount: Int
+    get() {
+        return 0
+    }
+
+val Int.parity: Int
+    get() {
+        return 0
+    }
---
+++ kotlin/algorithms/AllAlgorithmsCP/src/com/br/cp/book/cphandbook/chp10/shift/shift.kt
@@ -0,0 +1,44 @@
+package src.com.br.cp.book.cphandbook.chp10.shift
+
+infix fun Int.isSet(i: Int) = (this and (1 shl i)) > 0
+
+val Int.binaryString: String
+    get() {
+        val builder = StringBuilder()
+        for (i in 31 downTo 0) {
+            builder.append(if (this isSet i) "1" else "0")
+        }
+        return builder.toString()
+    }
+
+
+private fun check1() {
+    val k = 31
+    val range = ((1 shl k) - 2) .. (1 shl k) -1
+
+    range.forEach {
+        println("$it: ${it.binaryString} ${it.binaryString.length}")
+    }
+}
+
+private fun check2() {
+    fun checkShift(x: Int): String {
+        val builder = StringBuilder()
+        for (i in 31 downTo 0) {
+            //builder.append( if(x and (1 shl i) > 0) "1" else "0")
+            builder.append(if (x isSet i) "1" else "0")
+        }
+        return builder.toString()
+    }
+
+    val k = 30
+    val range = ((1 shl k) - 2) .. (1 shl  k) -1
+
+    range.forEach {
+        println("$it: ${checkShift(it)}")
+    }
+}
+
+fun main() {
+    check1()
+}
