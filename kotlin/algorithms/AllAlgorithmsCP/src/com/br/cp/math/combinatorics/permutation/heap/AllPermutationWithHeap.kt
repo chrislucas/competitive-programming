0142aaacb1f0d337fc9f4a9362799506ea7c180b
christoffer lucas <christoffer.santos@mercadolivre.com>
2021-10-29 01:48:16 -0300

    estudo sobre permutacao

---
+++ kotlin/algorithms/AllAlgorithmsCP/src/com/br/cp/math/combinatorics/permutation/heap/AllPermutationWithHeap.kt
@@ -0,0 +1,8 @@
+package src.com.br.cp.math.combinatorics.permutation.heap
+
+/*
+    https://www.geeksforgeeks.org/heaps-algorithm-for-generating-permutations/
+    https://en.wikipedia.org/wiki/Heap%27s_algorithm
+    https://www.cs.princeton.edu/~rs/talks/perms.pdf
+ */
+
--- kotlin/algorithms/AllAlgorithmsCP/src/com/br/cp/math/combinatorics/permutation/package-info.java
+++ kotlin/algorithms/AllAlgorithmsCP/src/com/br/cp/math/combinatorics/permutation/package-info.java
@@ -6,4 +6,11 @@ https://www.geeksforgeeks.org/heaps-algorithm-for-generating-permutations/?ref=r
 
 next permutation
 https://www.geeksforgeeks.org/find-the-next-lexicographically-greater-word-than-a-given-word/
+https://leetcode.com/problems/next-permutation/
+https://www.tutorialspoint.com/next-permutation-in-python
+https://massivealgorithms.blogspot.com/2014/07/leetcode-next-permutation-darrens-blog.html
+https://www.cnblogs.com/grandyang/p/4428207.html
+
+Make all combinations of size k
+https://www.geeksforgeeks.org/make-combinations-size-k/
  */
