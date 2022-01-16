f1badd6f071f22e3f47fde6127491e8d5edc456f
chrislucas <christoffer.luccas@gmail.com>
2022-01-16 14:31:11 -0300

    links para estudo

---
+++ kotlin/algorithms/AllAlgorithmsCP/codeforces/src/main/kotlin/com/br/algo/geom/circle/package-info.java
@@ -0,0 +1,27 @@
+package com.br.algo.geom.circle;
+
+/**
+ *  Posições relativas entre duas circunferências
+ *  https://www.preparaenem.com/matematica/posicoes-relativas-entre-duas-circunferencias.htm
+ *
+ *  Circle-Line Intersection
+ *  https://mathworld.wolfram.com/Circle-LineIntersection.html
+ *  https://cp-algorithms.com/geometry/circle-line-intersection.html
+ *
+ *  Circle line-segment collision detection algorithm?
+ *  https://stackoverflow.com/questions/1073336/circle-line-segment-collision-detection-algorithm
+ *
+ *  Check if two given circles touch or intersect each other
+ *  https://www.geeksforgeeks.org/check-two-given-circles-touch-intersect/
+ *
+ *  Check if a line touches or intersects a circle
+ *  https://www.geeksforgeeks.org/check-line-touches-intersects-circle/?ref=lbp
+ *
+ *  Circle-circle intersection points
+ *  https://stackoverflow.com/questions/3349125/circle-circle-intersection-points
+ *
+ *
+ *  Intersecção de circunferências
+ *  https://www.obaricentrodamente.com/2011/06/interseccao-de-circunferencias.html
+ *
+ */
---
+++ kotlin/algorithms/AllAlgorithmsCP/src/com/br/google/preparation/interview/package-info.java
@@ -0,0 +1,6 @@
+package src.com.br.google.preparation.interview;
+
+/*
+        https://techdevguide.withgoogle.com/paths/data-structures-and-algorithms/
+        https://careers.google.com/students/
+ */
