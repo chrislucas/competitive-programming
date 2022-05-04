b8a7e67a2c32cd45a3574e60f526376e2f666582
chrislucas <christoffer.luccas@gmail.com>
2022-05-04 08:29:00 -0300

    problemas para ser resolvidos

---
+++ kotlin/algorithms/AllAlgorithmsCP/uri/src/main/kotlin/com/br/problems/adhoc/lvl5/p1678.kt
@@ -0,0 +1,9 @@
+package com.br.problems.adhoc.lvl5
+
+/**
+ * https://www.beecrowd.com.br/judge/en/problems/view/1678
+ * */
+
+fun main(args: Array<String>) {
+
+}
---
+++ kotlin/algorithms/AllAlgorithmsCP/uri/src/main/kotlin/com/br/problems/adhoc/lvl5/p2280.kt
@@ -0,0 +1,9 @@
+package com.br.problems.adhoc.lvl5
+
+/**
+ * https://www.beecrowd.com.br/judge/en/problems/view/2280
+ * */
+
+fun main(args: Array<String>) {
+
+}
