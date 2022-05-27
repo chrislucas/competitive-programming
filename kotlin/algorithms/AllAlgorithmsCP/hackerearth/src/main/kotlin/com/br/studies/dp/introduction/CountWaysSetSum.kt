e661e1533f2ba7ef8f665c0de56dd1f4cb7c2139
chrislucas <christoffer.luccas@gmail.com>
2022-05-27 07:25:12 -0300

    pd

---
+++ kotlin/algorithms/AllAlgorithmsCP/hackerearth/src/main/kotlin/com/br/studies/dp/introduction/CountWaysSetSum.kt
@@ -0,0 +1,20 @@
+package com.br.studies.dp.introduction
+
+/*
+    Let us say that you are given a number N,
+    you've to find the number of different ways to write it as the sum of 1, 3 and 4.
+
+    For example, if N = 5, the answer would be 6.
+
+    1 + 1 + 1 + 1 + 1
+    1 + 4
+    4 + 1
+    1 + 1 + 3
+    1 + 3 + 1
+    3 + 1 + 1
+ */
+
+
+fun main() {
+
+}
