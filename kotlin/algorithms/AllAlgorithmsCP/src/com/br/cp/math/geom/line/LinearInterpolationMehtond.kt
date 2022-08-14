71ea4232007638b52e149124be30cae6995ae80d
chrislucas <christoffer.luccas@gmail.com>
2022-08-14 12:53:12 -0300

    estudos sobre funcao de interpolacao linear

--- kotlin/algorithms/AllAlgorithmsCP/src/com/br/cp/math/geom/line/LinearInterpolationMehtond.kt
+++ kotlin/algorithms/AllAlgorithmsCP/src/com/br/cp/math/geom/line/LinearInterpolationMehtond.kt
@@ -3,3 +3,33 @@ package src.com.br.cp.math.geom.line
 /*
     https://www.codesansar.com/numerical-methods/linear-interpolation-method-algorithm.htm
  */
+
+
+private fun checkLerp() {
+
+    // lerp"https://en.wikipedia.org/wiki/Linear_interpolation
+    fun lerp(p: Double, q: Double, t: Double) = (1.0 - t) * p + q * t
+
+    arrayOf(
+        arrayOf(0.0, 5.0, 0.001),
+        arrayOf(0.0, 5.0, 0.01),
+        arrayOf(0.0, 5.0, 0.1),
+        arrayOf(0.0, 5.0, 0.1),
+    ).forEach {
+        val (p, q, t) = it
+
+        var counter = p
+        while (counter <= q) {
+            println(String.format("%.3f -> %.3f", counter, lerp(p, q, counter)))
+            counter += t
+        }
+
+        println("*************************************************************")
+    }
+
+
+}
+
+fun main() {
+    checkLerp()
+}
