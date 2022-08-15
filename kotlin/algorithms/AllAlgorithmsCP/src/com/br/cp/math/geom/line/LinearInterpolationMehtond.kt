831cd5ed26d2340317aa89f04bf05e0de221e2a7
chrislucas <christoffer.luccas@gmail.com>
2022-08-15 08:38:06 -0300

    estudos sobre interpolacao

--- kotlin/algorithms/AllAlgorithmsCP/src/com/br/cp/math/geom/line/LinearInterpolationMehtond.kt
+++ kotlin/algorithms/AllAlgorithmsCP/src/com/br/cp/math/geom/line/LinearInterpolationMehtond.kt
@@ -10,16 +10,24 @@ private fun checkLerp() {
     // lerp"https://en.wikipedia.org/wiki/Linear_interpolation
     fun lerp(p: Double, q: Double, t: Double) = (1.0 - t) * p + q * t
 
+    /*
+          Resultado interessante: https://docs.unity3d.com/ScriptReference/Vector3.Lerp.html
+          The value returned equals a + (b - a) * t (which can also be written a * (1-t) + b*t).
+            When t = 0, Vector3.Lerp(a, b, t) returns a.
+            When t = 1, Vector3.Lerp(a, b, t) returns b.
+            When t = 0.5, Vector3.Lerp(a, b, t) returns the point midway between a and b.
+     */
+
     arrayOf(
         arrayOf(0.0, 5.0, 0.001),
         arrayOf(0.0, 5.0, 0.01),
         arrayOf(0.0, 5.0, 0.1),
-        arrayOf(0.0, 5.0, 0.1),
+        arrayOf(0.0, 5.0, 0.2),
     ).forEach {
         val (p, q, t) = it
 
-        var counter = p
-        while (counter <= q) {
+        var counter = 0.0
+        while (counter <= 1.0) {
             println(String.format("%.3f -> %.3f", counter, lerp(p, q, counter)))
             counter += t
         }
@@ -27,6 +35,13 @@ private fun checkLerp() {
         println("*************************************************************")
     }
 
+}
+
+/*
+    https://www.youtube.com/watch?v=_QQU9OIPGYE
+ */
+
+private fun linearInterpolationProblem1() {
 
 }
 
--- kotlin/algorithms/AllAlgorithmsCP/src/com/br/cp/math/geom/line/package-info.java
+++ kotlin/algorithms/AllAlgorithmsCP/src/com/br/cp/math/geom/line/package-info.java
@@ -1,10 +1,27 @@
 package src.com.br.cp.math.geom.line;
 
 /*
+    Interpolacao
+    https://ic.ufal.br/professor/thales/icg/Aula1.2.pdf
+    https://edisciplinas.usp.br/pluginfile.php/4689221/mod_resource/content/1/Revisao%202%202019.pdf
+
     Interpolacao linear
+    https://www.youtube.com/watch?v=_QQU9OIPGYE
+    https://www.ufsj.edu.br/portal2-repositorio/File/nepomuceno/mn/12MN_Interpola.pdf
+
     https://www.portalsaofrancisco.com.br/matematica/interpolacao
     https://pt.wikihow.com/Interpolar
     https://www.codesansar.com/numerical-methods/linear-interpolation-method-algorithm.htm
     https://en.wikipedia.org/wiki/Linear_interpolation
     https://docs.unity3d.com/ScriptReference/Vector3.Lerp.html
+
+    algoritmos
+    https://www.ufsj.edu.br/portal2-repositorio/File/nepomuceno/mn/12MN_Interpola.pdf
+    https://pt.khanacademy.org/partner-content/pixar-latam/animation-latam/mathematics-of-animation-curves-latam/v/anim6#
+    https://www.dca.ufrn.br/~meneghet/FTP/MCEC/Transp05.pdf
+    https://www.ime.unicamp.br/~roman/courses/MS211/1s2020/interpolacao1.pdf
+    https://www.ime.unicamp.br/~roman/courses/MS211/1s2020/interpolacao1.pdf
+
+    https://brasilescola.uol.com.br/matematica/interpolacao-meios-geometricos.htm
+    https://www.novaconcursos.com.br/arquivos-digitais/erratas/14729/19072/regressao-interpolacao-extrapolacao-numericas.pdf
  */
