6987eaceba62252a30fe59fca2ae54d0a6056b89
christoffer lucas <christoffer.santos@mercadolivre.com>
2024-04-30 19:01:41 -0300

    estudos

---
+++ kotlin/algorithms/AllAlgorithmsCP/codeforces/src/main/kotlin/com/br/competitions/problemsset/package-info.java
@@ -0,0 +1,11 @@
+package codeforces.src.main.kotlin.com.br.competitions.problemsset;
+
+/**
+ * https://codeforces.com/problemset?order=BY_SOLVED_DESC&tags=trees
+ * https://codeforces.com/problemset?order=BY_SOLVED_DESC&tags=binary+search
+ *
+ * <p>bst https://codeforces.com/problemset/problem/1237/E
+ * https://codeforces.com/problemset/gymProblem/100499/E
+ * https://codeforces.com/problemset/problem/797/D https://codeforces.com/problemset/problem/1025/D
+ * https://codeforces.com/blog/entry/113032 https://codeforces.com/contest/675/problem/D
+ */
